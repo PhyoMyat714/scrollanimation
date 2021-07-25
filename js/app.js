1749b277bcda052b6b7c7329cee21e0181f421dd
PhyoMyat714 <phyomyat714@gmail.com>
2021-07-25 15:40:15 +0630

    25/July/2020

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const boxes = document.querySelectorAll(".box");
+//console.log(boxes);
+
+function checkboxes(){
+
+//console.log(window.innerHeight);
+
+	const innerheight = window.innerHeight / 5 * 4;
+	console.log(innerheight);
+
+	boxes.forEach(box=>{
+
+		//console.log(box);
+
+		const boxtop = box.getBoundingClientRect().top;
+		//console.log(boxtop);
+
+		if(boxtop<innerheight){
+
+			box.classList.add('show');
+		}else{
+			box.classList.remove('show');
+		}
+
+	});
+
+}
+
+checkboxes();
+
+window.addEventListener('scroll',checkboxes);
